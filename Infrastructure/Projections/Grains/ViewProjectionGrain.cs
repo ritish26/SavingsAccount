60b18be306dcbdb330ef4c981a7860ae56e1f13a
Riickkyy <104491156+Riickkyy@users.noreply.github.com>
2025-03-01 00:36:44 +0530

    Code fix

--- Infrastructure/Projections/Grains/ViewProjectionGrain.cs
+++ Infrastructure/Projections/Grains/ViewProjectionGrain.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using Domain.Events;
 using Infrastructure.EventStore;
-using Microsoft.Extensions.Configuration;;
+using Microsoft.Extensions.Configuration;
 using Projections;
 
 namespace Infrastructure.Projections.Grains;
